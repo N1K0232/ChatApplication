b9ee52455aff375d2d7e01886ecb9af0543d3a63
N1K0232 <ns.nicolasilvestri@libero.it>
2024-01-24 21:27:57 +0100

    Add swagger integration

--- src/ChatApplication/Program.cs
+++ src/ChatApplication/Program.cs
@@ -1,6 +1,14 @@
+using System.Diagnostics;
 using ChatApplication.BusinessLayer.Settings;
+using ChatApplication.ExceptionHandlers;
+using ChatApplication.Extensions;
+using ChatApplication.Swagger;
+using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using OperationResults.AspNetCore;
 using TinyHelpers.AspNetCore.Extensions;
+using TinyHelpers.AspNetCore.Swagger;
 
 var builder = WebApplication.CreateBuilder(args);
 ConfigureServices(builder.Services, builder.Configuration, builder.Environment, builder.Host);
@@ -15,6 +23,41 @@ void ConfigureServices(IServiceCollection services, IConfiguration configuration
     var appSettings = services.ConfigureAndGet<AppSettings>(configuration, nameof(AppSettings));
     var swaggerSettings = services.ConfigureAndGet<SwaggerSettings>(configuration, nameof(SwaggerSettings));
 
+    services.AddHttpContextAccessor();
+    services.AddMemoryCache();
+
+    services.AddExceptionHandler<DefaultExceptionHandler>();
+    services.AddProblemDetails(options =>
+    {
+        options.CustomizeProblemDetails = context =>
+        {
+            var statusCode = context.ProblemDetails.Status.GetValueOrDefault(StatusCodes.Status500InternalServerError);
+            context.ProblemDetails.Type ??= $"https://httpstatuses.io/{statusCode}";
+            context.ProblemDetails.Title ??= ReasonPhrases.GetReasonPhrase(statusCode);
+            context.ProblemDetails.Instance ??= context.HttpContext.Request.Path;
+            context.ProblemDetails.Extensions["traceId"] = Activity.Current?.Id ?? context.HttpContext.TraceIdentifier;
+        };
+    });
+
+    services.AddWebOptimizer(minifyCss: true, minifyJavaScript: environment.IsProduction());
+    services.AddRequestLocalization(appSettings.SupportedCultures);
+
+    services.AddOperationResult(options =>
+    {
+        options.ErrorResponseFormat = ErrorResponseFormat.List;
+    });
+
+    if (swaggerSettings.Enabled)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo { Title = "ChatApplication API", Version = "v1" });
+            options.AddAcceptLanguageHeader();
+            options.AddDefaultResponse();
+        });
+    }
+
     services.AddControllers();
     services.AddRazorPages();
 }
@@ -27,15 +70,41 @@ void Configure(IApplicationBuilder app, IWebHostEnvironment environment, IServic
     environment.ApplicationName = appSettings.ApplicationName;
 
     app.UseHttpsRedirection();
+    app.UseRequestLocalization();
+
+    app.UseWhen(context => context.IsWebRequest(), builder =>
+    {
+        if (!environment.IsDevelopment())
+        {
+            builder.UseExceptionHandler("/errors/500");
+            builder.UseHsts();
+        }
+
+        builder.UseStatusCodePagesWithReExecute("/errors/{0}");
+    });
+
+    app.UseWhen(context => context.IsApiRequest(), builder =>
+    {
+        builder.UseExceptionHandler();
+        builder.UseStatusCodePages();
+    });
+
+    app.UseDefaultFiles();
     app.UseStaticFiles();
 
-    if (!environment.IsDevelopment())
+    if (swaggerSettings.Enabled)
     {
-        app.UseExceptionHandler("/Error");
-        app.UseHsts();
+        app.UseMiddleware<SwaggerAuthenticationMiddleware>();
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "ChatApplication API v1");
+            options.InjectStylesheet("/css/swagger.css");
+        });
     }
 
     app.UseRouting();
+    app.UseWebOptimizer();
 
     app.UseAuthentication();
     app.UseAuthorization();
